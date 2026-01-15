d79c78fda0f7450c14c6dac5d8f5cd9c0cbcdac5
Esdras <esdras.diogo@fagrontech.com.br>
2026-01-15 16:15:04 -0300

    Adicionando prod

--- src/app/services/gastos.service.ts
+++ src/app/services/gastos.service.ts
@@ -1,5 +1,5 @@
 import { Ano } from './../models/ano.model';
-import { environment } from './../enviroment/enviroment';
+import { environment } from '../../enviroment/enviroment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
