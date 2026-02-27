35e98eae3d8f57a1276af0ad315a7a6e9beaa56a
Esdras Diogo <Esdras.Diogo@fagrontech.com.br>
2026-02-27 13:20:07 -0400

    Gerando o build para produção

--- src/enviroment/enviroment.production.ts
+++ src/enviroment/enviroment.production.ts
@@ -8,6 +8,8 @@ export const environment = {
     cadastro: 'https://backend-gerenciamento-gastos.onrender.com/api/gastos/cadastro',
     buscarDespesa: 'https://backend-gerenciamento-gastos.onrender.com/api/gastos/buscarDespesa',
     atualizarDespesa: 'https://backend-gerenciamento-gastos.onrender.com/api/gastos/atualizarDespesa',
-    deletarDespesa: 'https://backend-gerenciamento-gastos.onrender.com/api/gastos/deletarDespesa'
+    deletarDespesa: 'https://backend-gerenciamento-gastos.onrender.com/api/gastos/deletarDespesa',
+    registarUsuario: 'https://backend-gerenciamento-gastos.onrender.com/api/auth/registro',
+    buscarUsuario: 'https://backend-gerenciamento-gastos.onrender.com/api/auth/buscarUsuario/'
   }
 };
