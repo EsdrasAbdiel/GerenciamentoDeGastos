2117e945d1839cb7eadf50f45c95f76f94a62bf5
Esdras Diogo <Esdras.Diogo@fagrontech.com.br>
2026-03-01 15:16:47 -0400

    Adicionando o logout e autenticação de rotas

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { credentialsInterceptor } from './app/interceptors/credentials.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor])
+    ),
 
     ...(appConfig.providers ?? [])
   ]
